30897ae7a812240fc234dacfa4b2ccb4b3057697
dmoracze <dmoracze@gmail.com>
2019-02-18 12:56:57 -0500

    better commenting on R script

--- condition_motion/CHT_condition_motion_analysis.R
+++ condition_motion/CHT_condition_motion_analysis.R
@@ -1,7 +1,10 @@
+########################### LIBRARIES ###########################
 library(ggplot2)
 library(reshape2)
 library(lme4)
 
+########################### FUNCTIONS ###########################
+# this function will read in timing and motion files from afni_cond_motion_analysis and create a condition-specific data frame for statistical analysis
 CHT_motion <- function(dir,subj,grp,part,event) {
 	n <- length(subj)*length(part)*length(event) # number of rows
 	s <- vector('character',n) # subjects
@@ -10,39 +13,41 @@ CHT_motion <- function(dir,subj,grp,part,event) {
 	g <- vector('character',n) # group (probably should just be 1)
 	m <- vector('numeric',n) # motion
 	count <- 0
+	# loop through the subjects
 	for (ss in 1:length(subj)) {
 		cat(subj[ss],'\n')
-		# read in subjects motion
+		# read in subject's motion
 		mot <- read.table(paste0(dir,'/mopar/',subj[ss],'_CHTall.enorm.1D'))
+		# loop through the chat partners
 		for (pp in 1:length(part)) {
+			# loop through the event types
 			for (ee in 1:length(event)) {
 				count <- count+1
 				# read in stim file
 				tim <- read.table(paste0(dir,'/times/',subj[ss],'.',part[pp],'.',event[ee],'.1D'))
-				# count number of TRs
-				nT <- length(which(tim==1))
-				# multiply sitm file by motion
-				inter <- mot*tim
-				s[count] <- subj[ss]
-				p[count] <- part[pp]
-				e[count] <- event[ee]
-				g[count] <- grp
+				nT <- length(which(tim==1)) # count number of TRs
+				inter <- mot*tim # multiply sitm file by motion
+				s[count] <- subj[ss] # record subject
+				p[count] <- part[pp] # record partner
+				e[count] <- event[ee] # record event
+				g[count] <- grp # record group
 				m[count] <- sum(inter)/nT # sum of FD / number of TRs
 			}
 		}
 	}
+	# put it all together
 	fin <- data.frame(subj=s,partner=p,event=e,group=g,meanFD=m)
 	return(fin)
 }
 
 
+########################### ANALYSIS ###########################
 dir <- '/export/data/brain1/CHT/Dustin/gPPI/dissertation/condition_motion'
 part <- c('peer','comp')
 event <- c('init','agree','away')
 
 
 nt <- c('RED_CAT_104', 'RED_CAT_107', 'RED_CAT_109', 'RED_CAT_110', 'RED_CAT_111', 'RED_CAT_115', 'RED_CAT_116', 'RED_CAT_117', 'RED_CAT_118', 'RED_CAT_123', 'RED_CAT_124', 'RED_CAT_127', 'RED_CAT_128', 'RED_CAT_132', 'RED_CAT_133', 'RED_CAT_134', 'RED_CAT_135', 'RED_CAT_139', 'RED_CAT_140', 'RED_CAT_142', 'RED_CAT_144', 'RED_CAT_145', 'RED_CAT_147', 'RED_CAT_148', 'RED_CAT_149', 'RED_CAT_150', 'RED_CAT_155', 'RED_CAT_157', 'RED_CAT_160', 'RED_CAT_164', 'RED_CAT_168', 'RED_CAT_170', 'RED_CAT_173', 'RED_CAT_174', 'RED_CHT_ASD_168', 'RED_CHT_ASD_169', 'RED_CHT_ASD_170', 'RED_CHT_ASD_171', 'RED_CHT_ASD_177', 'RED_LL_167', 'RED_LL_187', 'RED_RL_106', 'RED_RL_111', 'RED_RL_118', 'RED_RL_119', 'RED_RL_124', 'RED_RL_126', 'RED_RL_128', 'RED_RL_130', 'RED_RL_146', 'RED_RL_150', 'RED_RL_151', 'RED_RL_152', 'RED_RL_153', 'RED_RL_154', 'RED_RL_155', 'RED_RL_157', 'RED_RL_158', 'RED_RL_159', 'RED_RL_160', 'RED_RL_162', 'RED_RL_163', 'RED_RL_165')
-
 asd <- c('RED_CHT_ASD_224', 'RED_CAT_228', 'RED_CAT_224', 'RED_CAT_241', 'RED_CHT_ASD_212', 'RED_CAT_229', 'RED_CHT_ASD_215', 'RED_CHT_ASD_225', 'RED_CAT_205', 'RED_CAT_248', 'RED_CHT_ASD_211', 'RED_CAT_220', 'RED_CHT_ASD_220', 'RED_CAT_216', 'RED_CHT_ASD_201', 'RED_CAT_234', 'RED_CAT_218', 'RED_CAT_212', 'RED_CHT_ASD_223', 'RED_CHT_ASD_202', 'RED_CHT_ASD_227', 'RED_CHT_ASD_209', 'RED_CHT_ASD_216', 'RED_CAT_230', 'RED_CAT_226', 'RED_CHT_ASD_218', 'RED_CHT_ASD_213', 'RED_CAT_217', 'RED_CAT_242', 'RED_CAT_201', 'RED_CAT_207')
 
 
